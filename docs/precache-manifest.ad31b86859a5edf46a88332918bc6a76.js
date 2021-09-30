9ae4e2b763208adbbd086698a6ff40bb1b53c4e9
jespadas <julio.espadas@gmail.com>
2021-09-30 14:37:56 +0200

    gh build

---
+++ docs/precache-manifest.ad31b86859a5edf46a88332918bc6a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e0977818e1aff9eacba1a389510746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7c8d7adbe9d395c893",
+    "url": "/static/css/main.81837c64.chunk.css"
+  },
+  {
+    "revision": "913f893746d53ef8e683",
+    "url": "/static/js/2.748cbe04.chunk.js"
+  },
+  {
+    "revision": "bc959fd919f7b4891d2595c7d5e43dcb",
+    "url": "/static/js/2.748cbe04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7c8d7adbe9d395c893",
+    "url": "/static/js/main.87569f7c.chunk.js"
+  },
+  {
+    "revision": "03ffa1be770947cc014b",
+    "url": "/static/js/runtime-main.6dcd31d5.js"
+  }
+]);
